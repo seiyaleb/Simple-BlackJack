a4c3d7085318ddd2c16f797d990e3d91e9b05841
seiya <seiyaichi1999leb@gmail.com>
2020-12-07 00:06:42 +0900

    add a new file

---
+++ src/main/java/com/example/demo/SimpleBlackJackApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBlackJackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBlackJackApplication.class, args);
+	}
+
+}
